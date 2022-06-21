dc6679fc154690542010b47d15d993d8d1ac2ac8
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2022-06-21 21:25:19 +0200

    instances created

--- library.js
+++ library.js
@@ -1,29 +1,113 @@
-class media{
+class Media{
     constructor(title){
-    title = this._title;
-    isCheckedOut = true;
-    ratings = [];
+    this._title = title;
+    this._isCheckedOut = true;
+    this._rating = [];
     }
 
 
     get title() {
-        return this._title;
+      return this._title; //don't forget to return
     }
+    set isCheckedOut(newStatus){
+        this._isCheckedOut = newStatus;
+  }
 
     get isCheckedOut() {
-        
-        this._checkedOut
+      return this._isCheckedOut
 }
     get ratings(){
-        this.ratings;
+      return   this.ratings;
     }
 
     getAverageRating(){
-
+        let ratingsSum = this._rating.reduce((currentSum, rating) => currentSum + rating, 0);
+        const lengthOfArray = this._rating.length;
+        let avgRating = ratingsSum/lengthOfArray;
+         console.log(avgRating);
     }
 
-    toggleCheckOutStatus(){
-        
+     toggleCheckOutStatus(){
+       this._isCheckedOut = !this._isCheckedOut;
+    }
+
+    addRating(newRating){
+     this._rating.push(newRating)
+    }
+}
+
+class Book extends Media{
+    constructor(title, author, pages){
+    super(title);
+    this._author = author;
+    this._pages = pages;
+    }
+    
+    get author(){
+        return this._author;
+    }
+
+    get pages(){
+        return this._pages;
+    }
+    }
+
+
+class Movie extends Media{
+    constructor(title,director,runTime){
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+}
+    get director(){
+        return this._director;
+    }
+
+    get runTime(){
+        return this._runTime;
     }
 
 }
+
+class Cd extends Media{
+    constructor(title,artist,songs){
+    super(title);
+    this._artist = artist;
+    this._songs = songs;
+}
+    
+    get artist(){
+        return this._artist;
+    }
+
+    get songs(){
+        return this._songs;
+    }
+}
+
+
+const historyOfEverything = new Book ('A Short History of Nearly Everything','Bill Bryson',544);
+console.log(historyOfEverything);
+
+historyOfEverything.toggleCheckOutStatus();
+//console.log(historyOfEverything);
+//console.log(historyOfEverything.isCheckedOut);
+
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+
+historyOfEverything.getAverageRating();
+//console.log(historyOfEverything);
+
+const speed = new Movie('Speed','Jan de Bont', 116);
+//console.log(speed);
+
+speed.toggleCheckOutStatus();
+//console.log(speed.isCheckedOut);
+console.log(speed);
+
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+speed.getAverageRating();
