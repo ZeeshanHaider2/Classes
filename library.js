eb68d19021e1cd9beb4b5b4cc2d74c2ee0a82b1e
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2022-06-21 17:33:55 +0200

    methods added

--- library.js
+++ library.js
@@ -1,1 +1,29 @@
-console.log("Zeeshan");
+class media{
+    constructor(title){
+    title = this._title;
+    isCheckedOut = true;
+    ratings = [];
+    }
+
+
+    get title() {
+        return this._title;
+    }
+
+    get isCheckedOut() {
+        
+        this._checkedOut
+}
+    get ratings(){
+        this.ratings;
+    }
+
+    getAverageRating(){
+
+    }
+
+    toggleCheckOutStatus(){
+        
+    }
+
+}
