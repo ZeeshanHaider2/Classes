a1f09ee0ec8db73aee7ce98a87d20e4817d7647a
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2022-06-22 19:49:40 +0200

    instances created and methods called

--- catalogue.js
+++ catalogue.js
@@ -1,8 +1,71 @@
-//let randomTeacher = Math.floor (substituteTeachers.length * Math.random());
-  // console.log (substituteTeachers[randomTeacher]);
+class School {
+    constructor(name, level, noOfStudents){
+        this._name = name;
+        this._level= level;
+        this._noOfStudents = noOfStudents;
+    }
+    get name(){
+        return this._name;
+    }
+    get level(){
+        return this._level;
+    }
+    get noOfStudents(){
+        return this._noOfStudents;
+    }
 
-let choices = ['shani','naina','sara', 'afrid','zubair'];
+    set noOfStudents(noOfStudents){
+        if(typeof noOfStudents == 'number'){
+            this._noOfStudents = newNoOfStudents
+        } else{
+            console.log('invalid input, it must be a number!');
+        }
+    }
 
-let random = Math.floor(Math.random() * choices.length);
+     quickFacts(){
+       console.log(` ${this._name} school educates ${this._noOfStudents} at ${this._level}`);
+    }
+    static pickSubstituteTeacher(substituteTeachers){
+        let randomTeacher = Math.floor (substituteTeachers.length * Math.random());
+        console.log (substituteTeachers[randomTeacher]);
+       }
+    }
 
-console.log(choices[random]);
+    class PrimarySchool extends School{
+        constructor(name,noOfStudents,pickupPolicy){
+            super(name,'primary',noOfStudents);
+            this._pickupPolicy = pickupPolicy;
+        }
+        get pickupPolicy(){
+            return this.pickupPolicy;
+        }
+    }
+    class MiddleSchool extends School{
+        constructor(name,noOfStudents){
+            super(name,'middle', noOfStudents);
+        }
+    }
+
+    class HighSchool extends School{
+            constructor(name,noOfStudents,sportsTeams){
+                super(name,'high',noOfStudents);
+                this._sportsTeams = sportsTeams;
+            }
+            get sportsTeams(){
+                return this.sportsTeams;
+            }
+    }
+
+
+    const lorraineHansbury = new PrimarySchool('Lorraine Hansbury',514,'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+
+console.log(lorraineHansbury.quickFacts());
+
+console.log(lorraineHansbury.noOfStudents);
+//console.log(lorraineHansbury.pickupPolicy);
+
+School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field'] );
+
+console.log(alSmith._sportsTeams);
