c476072cd7500c5dc3c88f039bf294e3d7e63a60
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2022-06-22 14:48:57 +0200

    readme updates with task descriptions.

---
+++ catalogue.js
@@ -0,0 +1,8 @@
+//let randomTeacher = Math.floor (substituteTeachers.length * Math.random());
+  // console.log (substituteTeachers[randomTeacher]);
+
+let choices = ['shani','naina','sara', 'afrid','zubair'];
+
+let random = Math.floor(Math.random() * choices.length);
+
+console.log(choices[random]);
